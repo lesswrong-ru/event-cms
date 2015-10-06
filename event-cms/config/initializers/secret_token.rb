6d782bb71152c2cc07478a2f56a471317a7c4ada
Gitdes <iamgitdes@gmail.com>
2015-10-07 00:09:18 +0300

    Unignore config directory

---
+++ event-cms/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EventCms::Application.config.secret_key_base = '5a8dfbd63137a16fb87a0f5db22ac37f7a3bfb104af9ad126b4f86a9787b50954758c9cc60efac1c09246c8229a7fb8a79995e54a3e0d926a23163faef228282'
